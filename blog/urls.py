71cf485593b5db2ea4e2e4639df0a2176ac2492c
beerbank3 <yg050140528@gmail.com>
2023-06-21 16:17:16 +0900

    django-230621

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from blog.views import Index
+from . import views
+
+app_name = 'blog'
 
 urlpatterns = [
-    #path(패턴, 매핑)
-    path("", Index.as_view())
+    # path(패턴, 매핑) /blog/
+    # path("", views.index), # FBV
+    # 글 목록 조회
+    path("", views.List.as_view(), name='list'),
+    # 글 상세
+    path("detail/<int:pk>/", views.Detail.as_view(), name='detail'),
+    # 글 작성
+    path("write/", views.Write.as_view(), name='write'),
+    # 글 수정
+    # 글 삭제
+    # 코멘트 작성
+    # 코멘트 삭제
 ]
