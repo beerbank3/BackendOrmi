2b9a3e6ee3cfd55169c69fd7065b4fd45d8893e8
beerbank3 <yg050140528@gmail.com>
2023-07-03 16:45:53 +0900

    django -230703

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
+
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    writer = models.CharField(max_length=10)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
     content = models.TextField()
     writer = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return f'Comment on {self.post.title}'
 
@@ -21,6 +26,6 @@ class Comment(models.Model):
 class HashTag(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
-
+    
     def __str__(self):
         return self.name
